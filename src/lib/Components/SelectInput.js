4d4d9e830d6574197d55ac94efed709688aab1c2
Hugh Bellamy <hughbellars@gmail.com>
2019-03-16 12:06:36 +0000

    Allow children in SelectInput

--- src/lib/Components/SelectInput.js
+++ src/lib/Components/SelectInput.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-const SelectInput = ({ name, value, children }) => (
-    <select
-        className="form-control"
-        onChange={event => value.set(event.target.value)}
-        value={value.get()}
-    >
-        <option>{name}</option>
-        {children.map(c => <option key={c}>{c}</option>)}
-    </select>
-);
-export default SelectInput;
+function getChildren(children) {
+    if (!Array.shift(children) || children.length === 0) {
+        return children;
+    }
+
+    // Convert an array of strings into an array of options.
+    if (typeof children[0] === 'string' || children[0] instanceof String) {
+        return children.map(c => <option key={c}>{c}</option>);
+    }
+
+    return children;
+}
+
+export default function SelectInput({ name, value, children, ...rest }) {
+    return (
+        <select
+            className="form-control"
+            onChange={event => value.set(event.target.value)}
+            value={value.get()}
+            {...rest}
+        >
+            <option>{name}</option>
+            {getChildren(children)}
+        </select>
+    );
+}
